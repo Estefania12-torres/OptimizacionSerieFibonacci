5967248cafaba892537f46ac24a9248c5f6a14b1
Abel214 <131829158+Abel214@users.noreply.github.com>
2023-06-12 21:53:06 -0500

    Update main.java
    Comentario main 1.0

--- main.java
+++ main.java
@@ -6,7 +6,7 @@ public class Main { //Se define la clase main
     public Main() { //Constructor
     }
     public static void main(String[] args) {
-        Serie serie = new Serie();
+        Serie serie = new Serie(); //Se crea un nuevo objeto
         System.out.println(serie.getFibonacci(50));
         System.out.println(serie);
     }
