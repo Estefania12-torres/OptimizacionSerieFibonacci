3f6fad2b1a79c8a215a421e3f1d7409daca8cf45
Estefania12-torres <133797333+Estefania12-torres@users.noreply.github.com>
2023-06-12 21:04:05 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,13 @@
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
 
+public class Main {
+    public Main() {
+    }
+    public static void main(String[] args) {
+        Serie serie = new Serie();
+        System.out.println(serie.getFibonacci(50));
+        System.out.println(serie);
+    }
+}
