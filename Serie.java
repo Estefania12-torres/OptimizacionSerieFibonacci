2f810c6f24061f5faaf10889f377f10d81014b36
Estefania12-torres <133797333+Estefania12-torres@users.noreply.github.com>
2023-06-12 21:03:00 -0500

    Update Serie.java

--- Serie.java
+++ Serie.java
@@ -1,1 +1,52 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Serie {
+    private Map<Integer, Long> valorList = new HashMap();
+
+    public Serie() {
+    }
+
+    public long getFibonacci(int position) {
+        if (position != 0 && position != 1) {
+            if (this.valorList.containsKey(position)) {
+                return (Long)this.valorList.get(position);
+            } else {
+                long resultado = this.getFibonacci(position - 1) + this.getFibonacci(position - 2);
+                this.valorList.put(position, resultado);
+                return resultado;
+            }
+        } else {
+            return 1;
+        }
+    }
+    
+    //Get y Set
+    public Map<Integer, Long> getValorList() {
+        return this.valorList;
+    }
+  
+    public void setValorList(Map<Integer, Long> valorList) {
+        this.valorList = valorList;
+    }
+    //constructor
+    private void printValorList() {
+        StringBuilder builder = new StringBuilder();
+
+        Map.Entry entry;
+        for(Iterator var2 = this.valorList.entrySet().iterator(); var2.hasNext(); builder.append(entry.getValue())) {
+            entry = (Map.Entry)var2.next();
+            if (builder.length() > 0) {
+                builder.append(" , ");
+            }
+        }
+
+        System.out.println(builder.toString());
+    }
+    //toString
+    public String toString() {
+        return "Serie: " + this.valorList.toString();
+    }
+}
 
